42a8b76e230fa629590a5849664f4da89911e9d9
baksakcci <pshsh910@gmail.com>
2022-08-05 12:49:04 +0900

    feat: BeanScopeTest

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,51 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/*
+프로토타입 빈 스코프 테스트
+
+1. 빈 스코프를 prototype 으로 지정
+2. init이 개별로 출력 되는지,
+   같은 객체를 가져와서 저장할 때 다른 객체체 반환하는지,
+   생명주기 콜백을 사용해 destroy() 종료 메서드는 호출 안되는지 확인
+ */
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        // 종료 메서드를 수동으로 들고와야 한다.
+        // prototypeBean1.destroy();
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,48 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/*
+싱글톤 빈 스코프 테스트
+
+1. 빈 스코프를 singleton으로 지정
+2. 같은 객체를 가져와서 저장할 때 같은 객체를 반환하는지,
+   생명주기 콜백을 사용해 컨테이너 끝날때 까지 유지되는지 확인
+
+
+ */
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
